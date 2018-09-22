0674d23016a02c9ce8aaa6166f77c73b495f5806
Nazar94 <nazar.masliy94@gmail.com>
2018-09-22 19:11:26 +0300

    Parsing the body and Handling the CORS

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const app = express();
-//installed morgan
+//installed morgan using for hadling errors
 const morgan = require("morgan");
-
+//installed bodyparser
+const bodyParser = require('body-parser');
 
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+//use morgan
 app.use(morgan('dev'));
+//use bodyparser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//add before routes CORS(adding headers)
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');//'*' means to give the access to any client, may set only your donmain
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Authorization' //al these headers could be appended to our request
+     );
+     if(req.method==='OPTIONS'){
+         res.header('Acces-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');//What methods should to support with OPTIONS
+        return res.status(200).json({});
+        }
+    next();
+});
 
 //Routes which should handle requests
 app.use('/products', productRoutes);
